5f28e1010c98144c2d8c195935c7b69a166030d0
Anna <anut04ka2010@mail.ru>
2022-10-03 23:38:28 +0800

    .

--- HomeWork1/hw.py
+++ HomeWork1/hw.py
@@ -100,11 +100,9 @@
 
 x1 = int(input('Введите x1: '))
 y1 = int(input('Введите y1: '))
-z1 = int(input('Введите z1: '))
 x2 = int(input('Введите x2: '))
 y2 = int(input('Введите y2: '))
-z2 = int(input('Введите z2: '))
 
 from math import sqrt
-distance = sqrt(((x2-x1)**2)+((y2-y1)**2)+((z2-z1)**2))
+distance = sqrt(((x2-x1)**2)+((y2-y1)**2))
 print(distance)
