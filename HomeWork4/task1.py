2250c07247a41ab58efe8bc64eb1c12b5be1c40e
Anna <anut04ka2010@mail.ru>
2022-10-15 00:21:19 +0800

    .,.

--- HomeWork4/task1.py
+++ HomeWork4/task1.py
@@ -1,6 +1,24 @@
-Вычислить число ПИ c заданной точностью *d*
-Пример:
-при d = 0.001, π = 3.141
-при d = 0.1, π = 3.1
-при d = 0.00001, π = 3.14154
-d от 0.1 до 0.0000000001
+# Вычислить число ПИ c заданной точностью *d*
+# Пример:
+# при d = 0.001, π = 3.141
+# при d = 0.1, π = 3.1
+# при d = 0.00001, π = 3.14154
+# d от 0.1 до 0.0000000001
+
+import math
+from decimal import Decimal
+
+d = float(input("Введите шаблон для округления: "))
+
+def digit(num:float):
+    count = 0
+    div = 1
+    while True:
+        if not(num*div == int(num*div)):
+            count += 1
+            div *= 10
+        else: break
+    return count
+
+print(digit(d))
+print(round(math.pi, digit(d)))
--- HomeWork4/task3.py
+++ HomeWork4/task3.py
@@ -1,6 +1,28 @@
-Задайте последовательность цифр. Напишите программу, которая выведет список неповторяющихся элементов
-исходной последовательности.
-Пример:
-47756688399943 -> [5]
-1113384455229 -> [8,9]
-1115566773322 -> []
+# Задайте последовательность цифр. Напишите программу, которая выведет список неповторяющихся элементов
+# исходной последовательности.
+# Пример:
+# 47756688399943 -> [5]
+# 1113384455229 -> [8,9]
+# 1115566773322 -> []
+
+
+from random import randint as rI
+from re import I
+
+uniqueList = {}
+finalStr = ''
+
+listStr = "".join(list(map(str, [rI(0,9) for i in range(40)])))
+print(f'Последовательность цифр {listStr}')
+
+for c in listStr:
+    if uniqueList.get(c):
+        uniqueList[c] = uniqueList.get(c) + I
+    else:
+        uniqueList[c] = 1
+
+for i in uniqueList.items():
+    if i[1] == 1:
+        finalStr += str(i[0])
+
+print(f'Уникальная(ые) цифра(ы) {finalStr}') if finalStr else print('Уникальных позиций нет')
